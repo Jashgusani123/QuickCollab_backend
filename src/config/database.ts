7f0d9ac8480eb853a25498c10db6e5454626aa44
Jash Gusani <jashgusani78@gmail.com>
2025-12-22 21:24:59 +0530

    [check]: checking error from DB

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,8 @@ export const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI!);
     console.log("✅ MongoDB connected");
   } catch (error) {
+    console.log("MongoDB error: " + error);
+    
     console.log("❌ MongoDB connection failed");
     process.exit(1);
   }
