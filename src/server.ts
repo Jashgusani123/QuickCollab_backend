f3251e3185faabd581957d0a48a0afe994e49c92
Jash Gusani <jashgusani78@gmail.com>
2025-12-22 19:40:26 +0530

    [test]: give access to test

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000" , "https://quickcollab-ten.vercel.app"], credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/workspaces", workspaceRoutes);
