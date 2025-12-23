021551145be971010c94e96d3ac68546228d0cda
Jash Gusani <jashgusani78@gmail.com>
2025-12-23 23:10:37 +0530

    [test]:testing for bug

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.token;
+  console.log(token);
+  
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
